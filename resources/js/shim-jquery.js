3ac566f611693efdd263d544c712f2816c9b10c6
kamilsalim55 <1712502192@student.budiluhur.ac.id>
2025-04-29 10:43:39 +0700

    fix accordion hide cause bootstrap bundle

--- resources/js/shim-jquery.js
+++ resources/js/shim-jquery.js
@@ -5,9 +5,9 @@ import "pace-js";
 import datepickerFactory from "jquery-datepicker";
 datepickerFactory($)
 $.fn.select2 = select2
-import bootstrap from "bootstrap/dist/js/bootstrap.bundle.min.js";
+// import bootstrap from "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-window.bootstrap = bootstrap
+// window.bootstrap = bootstrap
 
 window.$ = $;
 window.jQuery = $;
