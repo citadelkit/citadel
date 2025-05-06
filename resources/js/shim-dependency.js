afa1069f6c625d5a7e6e792ac56c32717c3d14c0
Zun <malikal.j4@gmail.com>
2025-05-06 14:09:35 +0700

    Build 250506.2

--- resources/js/shim-dependency.js
+++ resources/js/shim-dependency.js
@@ -14,13 +14,13 @@ import '@support/dash-ui/src/assets/js/sidebarMenu.js';
 import 'autonumeric';
 import 'summernote';
 import Inputmask from 'inputmask';
+import Swal from 'sweetalert2';
 $.fn.inputmask = function (maskOrAlias, opts) {
     return this.each(function () {
         const im = new Inputmask(maskOrAlias, opts);
         im.mask(this);  
     });
 };
-
 window.$ = $;
 window.jQuery = $;
 window.jquery = $;
