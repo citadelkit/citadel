1bf213b1387db9965c4528d316b98dd169183b50
Zun <malikal.j4@gmail.com>
2025-05-13 11:31:51 +0700

    Restructure shim + Build 250513.2

--- resources/js/shim-dependency.js
+++ resources/js/shim-dependency.js
@@ -1,41 +1,35 @@
-import { $, jQuery } from './shim-jquery'
+// setup-legacy-globals.js
 
-import 'jquery-slimscroll/jquery.slimscroll.min.js';
+import './shim-jquery.js';
+
+import 'jquery-slimscroll';
 import 'jquery-loading-overlay';
-import 'prismjs/prism.js';
-import 'apexcharts/dist/apexcharts.min.js';
 import 'dropzone';
-import 'prismjs/plugins/toolbar/prism-toolbar.min.js';
-import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.min.js';
-import '@support/dash-ui/src/assets/js/main.js';
-import '@support/dash-ui/src/assets/js/feather.js';
-import '@support/dash-ui/src/assets/js/sidebarMenu.js';
-
-import 'autonumeric';
 import 'summernote';
-import toastr from 'toastr';
+import 'select2';
+
+// Bind plugins to jQuery
 import Inputmask from 'inputmask';
-import Swal from 'sweetalert2';
 import Select2 from 'select2';
+import toastr from 'toastr';
+import Swal from 'sweetalert2';
+
+window.Swal = Swal;
+window.swal = Swal;
+window.toastr = toastr;
+
 jQuery.fn.inputmask = function (maskOrAlias, opts) {
-    return this.each(function () {
-        const im = new Inputmask(maskOrAlias, opts);
-        im.mask(this);  
-    });
+  return this.each(function () {
+    const im = new Inputmask(maskOrAlias, opts);
+    im.mask(this);
+  });
 };
 jQuery.fn.select2 = function (opts) {
-    return this.each(function () {
-        new Select2(this, opts);
-    });
+  return this.each(function () {
+    new Select2(this, opts);
+  });
 };
 
 jQuery.loadingOverlay = () => {
-    console.log("Use $('.body').loadingOverlay()")
-}
-
-window.$ = jQuery;
-window.jQuery = jQuery;
-window.jquery = jQuery;
-window.Swal = Swal
-window.swal = Swal
-window.toastr = toastr
+  console.log("Use $('.body').loadingOverlay()");
+};
