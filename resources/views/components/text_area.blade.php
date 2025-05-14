6a93a06471db0906d5ba1411819c6c6c055dd087
kamilsalim55 <1712502192@student.budiluhur.ac.id>
2025-05-14 13:10:11 +0700

    fix element textarea

--- resources/views/components/text_area.blade.php
+++ resources/views/components/text_area.blade.php
@@ -1,6 +1,6 @@
 <div class="form-group" style="{{ $style['colspan'] }}">
     <label for="{{ $name }}" class="control-label ">{{ $title }}</label>
-    <textarea id="{{ $name }}" name="{{ $name }}" type="text" class="form-control" placeholder="{{ $placeholder }}"  rows="{{ $rows }}" value="{{ $value }}">
+    <textarea id="{{ $name }}" name="{{ $name }}" type="text" class="form-control" placeholder="{{ $placeholder }}"  rows="{{ $rows }}">{{ $value }}</textarea>
     <div class="error">
         <span class="text-danger"></span>
     </div>
