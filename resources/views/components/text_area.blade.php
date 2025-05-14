8f34883bb6b7cfce31cd7bee90ce227fe2e7fa4a
kamilsalim55 <1712502192@student.budiluhur.ac.id>
2025-05-14 10:39:54 +0700

    leftover

--- resources/views/components/text_area.blade.php
+++ resources/views/components/text_area.blade.php
@@ -1,7 +1,7 @@
 <div class="form-group" style="{{ $style['colspan'] }}">
-    <label for="input-{{ $name }}" class="control-label ">{{ $title }}</label>
-    <div class="input-group">
-            <input id="{{ $name }}" name="{{ $name }}" type="text" class="form-control" placeholder="{{ $placeholder }}"  value="{!! $value !!}" >
+    <label for="{{ $name }}" class="control-label ">{{ $title }}</label>
+    <textarea id="{{ $name }}" name="{{ $name }}" type="text" class="form-control" placeholder="{{ $placeholder }}" value="{!! $value !!}">
+    <div class="error">
+        <span class="text-danger"></span>
     </div>
-    <small class="text-danger mt-2"></small>
 </div>
