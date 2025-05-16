b5fbc7d2065b66197697f4288242e5a55b5e378b
kamilsalim55 <1712502192@student.budiluhur.ac.id>
2025-05-16 16:43:41 +0700

    update service tab to add direction

--- src/Components/Layout/Tabs.php
+++ src/Components/Layout/Tabs.php
@@ -8,29 +8,53 @@ use Citadel\Core\Wrapper;
 class Tabs extends Wrapper
 {
     protected $tabs = [];
+    protected $direction;
 
+
+    public function direction($direction = null)
+    {
+        $this->direction = $direction ?? 'row';
+
+        return $this; // return the object, not an array
+    }
+
+
+    public function getDirection()
+    {
+        return match ($this->direction) {
+            'column' => [
+                'header_direction' => 'd-flex',
+                'nav_direction' => 'flex-column',
+            ],
+            default => [
+                'header_direction' => '',
+                'nav_direction' => 'flex-row',
+            ],
+        };
+    }
     public function data()
     {
         $active = null;
         $schema = collect($this->schema)
-        ->filter(fn($c) => $c->isShown($this->pass_data))
-        ->map(function($item, $key) use (&$active) {
-            if(gettype($active) == "integer") return $item;
-            $disabled = $item->getDisabled();
-            if(!$disabled && $active == null) {
-                $item->active(true);
-                $active = $key;
-            }
-            return $item;
-        });
+            ->filter(fn($c) => $c->isShown($this->pass_data))
+            ->map(function ($item, $key) use (&$active) {
+                if (gettype($active) == "integer") return $item;
+                $disabled = $item->getDisabled();
+                if (!$disabled && $active == null) {
+                    $item->active(true);
+                    $active = $key;
+                }
+                return $item;
+            });
         return array_merge(
             parent::data(),
             [
                 'schema' => $schema,
                 // 'content' => $this->renderSchema(),
+                'direction' => $this->getDirection(),
                 'inject' => fn(Tab $component) => $component
-                ->setLifecycle('backbone')
-                ->passData($this->pass_data)
+                    ->setLifecycle('backbone')
+                    ->passData($this->pass_data)
             ]
         );
     }
