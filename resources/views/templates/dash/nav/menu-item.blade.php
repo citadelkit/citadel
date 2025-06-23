e73c9fa61a99e38f0a54b1dd8bda9173010041bf
kamilsalim55 <1712502192@student.budiluhur.ac.id>
2025-06-23 12:21:19 +0700

    test menu item

--- resources/views/templates/dash/nav/menu-item.blade.php
+++ resources/views/templates/dash/nav/menu-item.blade.php
@@ -3,7 +3,9 @@
         <a class="nav-link has-arrow {{ request()->is('pages/*') ? 'show' : 'collapsed' }}" href="#!"
             data-bs-toggle="collapse" data-bs-target="#nav-{{ strSnake($title) }}" aria-expanded="false"
             aria-controls="navPages">
-            {!! $icon !!} {{ $title }}
+            {{-- {!! $icon !!} {{ $title }} --}}
+             <span class="mb-1">{!! $icon !!}</span>
+            <span class="small text-wrap">{{ $title }}</span>
         </a>
         <div id="nav-{{ strSnake($title) }}" class="collapse {{ request()->is('pages/*') ? 'show' : '' }}"
             data-bs-parent="#{{ strSnake($parent) }}">
@@ -15,8 +17,10 @@
 @else
     <li class="nav-item">
         <a class="nav-link {{ request()->is('admin.tender.index') ? 'active' : '' }}"
-            href="{{ $href }}" target={{ $target }}>
-            {!! $icon !!} {{ $title }}
+            href="{{ $href }}"  target={{ $target }}>
+            {{-- {!! $icon !!} {{ $title }} --}}
+             <span class="mb-1">{!! $icon !!}</span>
+            <span class="small text-wrap">{{ $title }}</span>
         </a>
     </li>
 @endif
