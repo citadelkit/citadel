ad47edd753c1d7f977d634f2de64fcf2d80304ed
kamilsalim55 <1712502192@student.budiluhur.ac.id>
2025-08-15 14:46:13 +0700

    add before_submit on data leftover

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -1,4 +1,4 @@
-<form id="{{$name}}" class="form-group citadel-form {{ $style['class'] }}" style="{{ $style['colspan'] }} {{ $style['custom'] }}">
+<form id="{{$name}}" class="form-group citadel-form {{ $style['class'] }}" style="{{ $style['colspan'] }} {{ $style['custom'] }}" data-before-submit='@json($before_submit ?? [])'>
     <div class="" style="{{ $style['columns'] }}">
         {!! $html !!}
     </div>
