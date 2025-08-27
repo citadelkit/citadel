26759d88b6d0d1440e38200717d60303d287c57b
kamilsalim55 <1712502192@student.budiluhur.ac.id>
2025-08-27 13:26:18 +0700

    update menu-item active and show

--- resources/views/templates/dash/nav/menu-item.blade.php
+++ resources/views/templates/dash/nav/menu-item.blade.php
@@ -1,13 +1,30 @@
+@props(['title', 'href' => null, 'icon' => null, 'parent' => null, 'target' => null])
+
+@php
+    // Child item active: exact match
+    $isActive = $href && request()->is(ltrim($href, '/'));
+
+    // Parent active: one of its children matches current URL
+    $isParentActive = false;
+    if ($slot->hasActualContent()) {
+        // crude but works: search current path inside rendered children HTML
+        $isParentActive = Str::contains($slot, request()->path());
+    }
+@endphp
+
 @if($slot->hasActualContent())
     <li class="nav-item">
-        <a class="nav-link has-arrow {{ request()->is('pages/*') ? 'show' : 'collapsed' }}" href="#!"
-            data-bs-toggle="collapse" data-bs-target="#nav-{{ strSnake($title) }}" aria-expanded="false"
+        <a class="nav-link has-arrow {{ $isActive ? '' : 'collapsed' }}" href="#!"
+            data-bs-toggle="collapse" 
+            data-bs-target="#nav-{{ strSnake($title) }}" 
+            aria-expanded="{{ $isParentActive ? 'true' : 'false' }}"
             aria-controls="navPages">
             {{-- {!! $icon !!} {{ $title }} --}}
              <span class="mb-1">{!! $icon !!}</span>
             <span class="small text-wrap">{{ $title }}</span>
         </a>
-        <div id="nav-{{ strSnake($title) }}" class="collapse {{ request()->is('pages/*') ? 'show' : '' }}"
+        <div id="nav-{{ strSnake($title) }}" 
+            class="collapse {{ $isParentActive ? 'show' : '' }}"
             data-bs-parent="#{{ strSnake($parent) }}">
             <ul class="nav flex-column">
                 {{ $slot }}
@@ -16,7 +33,7 @@
     </li>
 @else
     <li class="nav-item">
-        <a class="nav-link {{ request()->is('admin.tender.index') ? 'active' : '' }}"
+        <a class="nav-link {{ $isActive ? 'active' : '' }}"
             href="{{ $href }}"  target={{ $target }}>
             {{-- {!! $icon !!} {{ $title }} --}}
              <span class="mb-1">{!! $icon !!}</span>
